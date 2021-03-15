b659544044e7306c11413ed5a554046bcc70a04d
Federico Mazzini <federico.mazzini3@studio.unibo.it>
2021-03-15 13:51:23 +0100

    first implementation ex 1.b

--- src/lab/Lists.scala
+++ src/lab/Lists.scala
@@ -1,12 +1,19 @@
 package lab
-import u03.Lists.List.Cons
+import u03.Lists.List.{Cons, append}
 import u03.Lists._
 
 object Lists {
 
 def drop[A](l: List[A], n: Int): List[A] = l match {
-  case Cons(h,t) if (n > 0) => drop(t, n - 1)
+  case Cons(_,t) if (n > 0) => drop(t, n - 1)
   case _ => l
 }
+
+  def flatMap[A,B](l: List[A])(f: A => List[B]): List[B] = l match {
+    case Cons(h, t) => append(f(h), flatMap(t)(f))
+    case List.Nil() => List.Nil()
+  }
+
+
 
 }
--- test/lab/ListTest.scala
+++ test/lab/ListTest.scala
@@ -15,4 +15,14 @@ object ListTest {
     assertEquals(Cons(30,List.Nil()), drop(lst, 2))
     assertEquals(List.Nil(), drop(lst, 5))
   }
+
+  @Test def testNegativeDrop() {
+    assertEquals(lst, drop(lst, -1))
+  }
+
+  @Test def testFlatMap() {
+    assertEquals(Cons(11,Cons(21,Cons(31,List.Nil()))), flatMap(lst)(v => Cons(v + 1 , List.Nil())))
+    assertEquals(Cons(11,Cons(21,Cons(31,List.Nil()))), flatMap(lst)(v => Cons(v+1, Cons(v+2, List.Nil()))))
+  }
+
 }
