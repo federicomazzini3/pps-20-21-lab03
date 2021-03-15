fa7662b7a0a9ddede7b2ec71e24aadead18c8583
Federico Mazzini <federico.mazzini3@studio.unibo.it>
2021-03-15 15:46:18 +0100

    first implementation of ex 2

--- src/lab/Lists.scala
+++ src/lab/Lists.scala
@@ -1,19 +1,38 @@
 package lab
+
 import u03.Lists.List.{Cons, Nil, append}
 import u03.Lists._
+import u02.Optionals._
 
 object Lists {
 
-def drop[A](l: List[A], n: Int): List[A] = l match {
-  case Cons(_,t) if (n > 0) => drop(t, n - 1)
-  case _ => l
-}
+  def drop[A](l: List[A], n: Int): List[A] = l match {
+    case Cons(_, t) if (n > 0) => drop(t, n - 1)
+    case _ => l
+  }
 
-  def flatMap[A,B](l: List[A])(f: A => List[B]): List[B] = l match {
+  def flatMap[A, B](l: List[A])(f: A => List[B]): List[B] = l match {
     case Cons(h, t) => append(f(h), flatMap(t)(f))
     case List.Nil() => List.Nil()
   }
 
-  def map[A,B](l: List[A])(mapper: A=>B): List[B] = flatMap(l)(a => Cons(mapper(a),List.Nil()))
+  def map[A, B](l: List[A])(mapper: A => B): List[B] = flatMap(l)(a => Cons(mapper(a), List.Nil()))
 
+  def filter[A](l: List[A])(pred: A => Boolean): List[A] = flatMap(l) {
+    case h if pred(h) => Cons(h,Nil())
+    case _ => List.Nil()
+  }
+
+  def max(l:List[Int]): Option[Int] = {
+    @annotation.tailrec
+    def _max(l:List[Int], n:Int): Option[Int] = l match{
+      case Cons(h,t) if(h > n) => _max(t, h)
+      case Cons(h,t) => _max(t, n)
+      case _ => Option.Some(n)
+    }
+    l match {
+      case Cons(h,t) => _max(t, h)
+      case _ => Option.None()
+    }
+  }
 }
--- test/lab/ListTest.scala
+++ test/lab/ListTest.scala
@@ -3,12 +3,14 @@ package lab
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import lab.Lists._
+import u02.Optionals._
 import u03.Lists.List
 import u03.Lists.List.Cons
 
 object ListTest {
 
   val lst = List.Cons(10, Cons(20, Cons(30,List.Nil())))
+  val lst2 = List.Cons(-10, Cons(-40, Cons(0, List.Nil())))
 
   @Test def testDrop(){
     assertEquals(Cons(20,Cons(30,List.Nil())), drop(lst, 1))
@@ -27,7 +29,15 @@ object ListTest {
 
   @Test def testMap(){
     assertEquals(Cons(11,Cons(21,Cons(31,List.Nil()))), map(lst)(_+1))
-
   }
 
+  @Test def testFilter(){
+    assertEquals(Cons(10,Cons(20, List.Nil())), filter(lst)(_<30))
+  }
+
+  @Test def testMax(): Unit ={
+    assertEquals(Option.Some(30), max(lst));
+    assertEquals(Option.Some(0), max(lst2));
+    assertEquals(Option.None(), max(List.Nil()))
+  }
 }
