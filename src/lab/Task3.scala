f09a22d757478bfab2050598752595bcc9612e8b
Federico Mazzini <federico.mazzini3@studio.unibo.it>
2021-03-18 12:05:08 +0100

    task 3 refactor

--- src/lab/Task3.scala
+++ src/lab/Task3.scala
@@ -4,8 +4,20 @@ import u03.Streams.Stream
 
 object Task3 {
 
+  /*
+  ex 5
+  go to method implementation called drop inside Streams object
+   */
+  def drop[A](stream: Stream[A])(n:Int): Stream[A] = Stream.drop(stream)(n)
+
+  /*
+   ex 6
+   */
   def constant[A](a: A): Stream[A] = Stream.cons(a, constant(a))
 
+  /*
+  ex 7
+   */
   def fibs: Stream[Long] = {
     def _fib(n1: Long, n2: Long): Stream[Long] = Stream.cons(n1+n2, _fib(n2, n1+n2))
     Stream.cons(0, Stream.cons(1,_fib(0,1)))
